dffea22f5215ff5294cbc2649f9e74c78f714a69
Ogni Goswami <ogni@ogniweb.com>
2019-01-08 22:15:26 -0600

    amit_recording

--- Beta Decay Capitalization.py
+++ Beta Decay Capitalization.py
@@ -7,13 +7,20 @@ def initialize(context):
     schedule_function(record_vars,date_rules.every_day(),time_rules.market_close(hours=0,minutes=1))
     
 def record_vars(context,data):
-    record(lever=context.account.leverage)
+    lever=context.account.leverage
+    #uncomment to record leverage
+    #record(lever=context.account.leverage)
+    bull_perc=abs((context.portfolio.positions[context.bull].amount*data.history(context.bull,'price',1,'1m')[0])/(context.portfolio.portfolio_value*lever))*100
+    bear_perc=abs((context.portfolio.positions[context.bear].amount*data.history(context.bear,'price',1,'1m')[0])/(context.portfolio.portfolio_value*lever))*100
+    #comment line below to record leverage
+    record(pos_spread=abs(bull_perc-bear_perc))
+    log.info('bull percentage=%s'%(bull_perc))
+    log.info('bear percentage=%s'%(bear_perc))
     
 def allocate(context,data):
-    bet_size = context.portfolio.portfolio_value * 3.9
+    leverage=3.9
+    bet_size = context.portfolio.portfolio_value * leverage
     context.bull_trade_amt=-((0.5*bet_size)/(data.history(context.bull,'price',1,'1m')[0]))-context.portfolio.positions[context.bull].amount
     context.bear_trade_amt=-((0.5*bet_size)/(data.history(context.bear,'price',1,'1m')[0]))-context.portfolio.positions[context.bear].amount
-    log.info(('context.bull_trade_amt=%s')%(context.bull_trade_amt))
-    log.info(('context.bear_trade_amt=%s')%(context.bear_trade_amt))
     order(context.bull,context.bull_trade_amt)
     order(context.bear,context.bear_trade_amt)
