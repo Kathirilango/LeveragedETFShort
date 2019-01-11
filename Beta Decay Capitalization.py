08ea7b5ba1b1e7a64a1ae9816ca66e8989b8c4c7
Ogni Goswami <ogni@ogniweb.com>
2019-01-11 15:27:50 -0800

    efficiency update

--- Beta Decay Capitalization.py
+++ Beta Decay Capitalization.py
@@ -3,38 +3,34 @@ from quantopian.pipeline import Pipeline,CustomFactor
 def initialize(context):
     context.bull=symbol('jnug')
     context.bear=symbol('jdst')
+    context.lever=context.account.leverage
+    #insert intended leverage below
+    context.truleverage=1.0
+    #insert max imbalance below
+    context.trupos_spread=5
+    bull_perc=abs((context.portfolio.positions[context.bull].amount*(data.current(context.bull,'price')))/(context.portfolio.portfolio_value*lever))*100
+    bear_perc=abs((context.portfolio.positions[context.bear].amount*(data.current(context.bear,'price')))/(context.portfolio.portfolio_value*lever))*100
+    context.pos_spread=abs(bull_perc-bear_perc)
+    
     schedule_function(allocate,date_rules.every_day(),time_rules.market_open(hours=3.5,minutes=0),half_days=True)
     schedule_function(record_vars,date_rules.every_day(),time_rules.market_close(hours=0,minutes=1),half_days=True)
     
 def record_vars(context,data):   
-    lever=context.account.leverage
-    # bull_perc=abs((context.portfolio.positions[context.bull].amount*(data.current(context.bull,'price')))/(context.portfolio.portfolio_value*lever))*100
-    # bear_perc=abs((context.portfolio.positions[context.bear].amount*(data.current(context.bear,'price')))/(context.portfolio.portfolio_value*lever))*100
-    
-    #write intended leverage ratio below for log generation
-    if lever>1.0:
-        log.warn('Leverage Exceeded: '+str(lever))
-    #record(pos_spread=abs(bull_perc-bear_perc))
-    record(leverage=lever)
+    if context.lever>context.truleverage:
+        log.warn('Leverage Exceeded: '+str(context.lever))
+    #record(imbalance=context.pos_spread)
+    record(leverage=context.lever)
 
 def allocate(context,data):
-    #edit leverage below (usually intended leverage ratio-0.1)
-    leverage=0.9
-    bet_size = context.portfolio.portfolio_value * leverage
+    bet_size = context.portfolio.portfolio_value * context.truleverage
     context.bull_trade_amt=-((0.5*bet_size)/(data.current(context.bull,'price')))-context.portfolio.positions[context.bull].amount
     context.bear_trade_amt=-((0.5*bet_size)/(data.current(context.bear,'price')))-context.portfolio.positions[context.bear].amount
     order(context.bull,context.bull_trade_amt)
     order(context.bear,context.bear_trade_amt)
     
 def handle_data(context,data):
-    lever=context.account.leverage
     try:
-        bull_perc=abs((context.portfolio.positions[context.bull].amount*(data.current(context.bull,'price')))/(context.portfolio.portfolio_value*lever))*100
-        bear_perc=abs((context.portfolio.positions[context.bear].amount*(data.current(context.bear,'price')))/(context.portfolio.portfolio_value*lever))*100
-        pos_spread=abs(bull_perc-bear_perc)
-        
-#edit number (5) below for managing spread
-        if pos_spread>5:
+        if context.pos_spread>context.trupos_spread:
             allocate(context,data)
     except:
         pass
