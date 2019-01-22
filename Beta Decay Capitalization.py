a1c3d6c23302dcd50e7e565be220c9e32985863a
Ogni Goswami <ogni@ogniweb.com>
2019-01-22 15:37:46 -0600

    works with JNUG/JDST

--- Beta Decay Capitalization.py
+++ Beta Decay Capitalization.py
@@ -1,16 +1,17 @@
 from quantopian.pipeline import Pipeline,CustomFactor
         
 def initialize(context):
-    context.bull=symbol('nugt')
-    context.bear=symbol('dust')
+    context.bull=symbol('jnug')
+    context.bear=symbol('jdst')
+    context.lever=context.account.leverage
     #insert interactive brokers commission below
     set_commission(commission.PerShare(cost=0.0035, min_trade_cost=0.35))
     #insert intended leverage below
-    context.truleverage=4
+    context.truleverage=1
     #insert max imbalance below
     context.trupos_spread=10
+    context.x=True
     context.open_orders = get_open_orders()  
-    context.time=get_datetime('US/Eastern').hour
     schedule_function(EOD,date_rules.every_day(),time_rules.market_close(hours=0,minutes=1),half_days=True)
     
 def EOD(context,data): 
@@ -18,6 +19,7 @@ def EOD(context,data):
     record(leverage=context.account.leverage)
     for equity in context.portfolio.positions:  
         order_percent(equity, 0)
+    context.x=True
     
 def allocate(context,data):
     if context.open_orders:
@@ -28,17 +30,19 @@ def allocate(context,data):
     context.bear_trade_amt=-((0.5*bet_size)/(data.current(context.bear,'price')))-context.portfolio.positions[context.bear].amount
     order(context.bull,context.bull_trade_amt)
     order(context.bear,context.bear_trade_amt)
+    context.x=False
     
 def handle_data(context,data):
     if len(context.portfolio.positions) > 0:
         bull_perc=abs((context.portfolio.positions[context.bull].amount*(data.current(context.bull,'price')))/(context.portfolio.portfolio_value))*100
         bear_perc=abs((context.portfolio.positions[context.bear].amount*(data.current(context.bear,'price')))/(context.portfolio.portfolio_value))*100
         context.pos_spread=abs(bull_perc-bear_perc)
-    if context.account.leverage>context.truleverage:
-            log.warn('Leverage Exceeded: '+str(context.account.leverage))
+    if context.lever>context.truleverage:
+            log.warn('Leverage Exceeded: '+str(context.lever))
             for equity in context.portfolio.positions:  
                 order_percent(equity, 0)
-    if len(context.portfolio.positions)==0 and context.time!=15:
+            context.x=True
+    if context.x==True:
         allocate(context,data)
     try:
         if context.pos_spread>context.trupos_spread:
