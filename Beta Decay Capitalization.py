02748ac9f9421f8db84557b527237c4efc595a96
Ogni Goswami <ogni@ogniweb.com>
2019-01-23 01:50:00 -0600

    1/23 update

--- Beta Decay Capitalization.py
+++ Beta Decay Capitalization.py
@@ -1,29 +1,54 @@
 from quantopian.pipeline import Pipeline,CustomFactor
+import numpy as np
         
 def initialize(context):
     context.bull=symbol('jnug')
     context.bear=symbol('jdst')
+    context.underlying=symbol('GDXJ')
     context.lever=context.account.leverage
     #insert interactive brokers commission below
     set_commission(commission.PerShare(cost=0.0035, min_trade_cost=0.35))
+    set_slippage(slippage.FixedSlippage(spread=0.00))
     #insert intended leverage below
     context.truleverage=1
     #insert max imbalance below
     context.trupos_spread=10
     context.x=True
     context.open_orders = get_open_orders()  
+    context.exchange_time = get_datetime('US/Eastern')
+    context.performance=[]
+    context.volatility=[]
     schedule_function(EOD,date_rules.every_day(),time_rules.market_close(hours=0,minutes=1),half_days=True)
     
 def EOD(context,data): 
-    record(imbalance=context.pos_spread)
-    record(leverage=context.account.leverage)
+    #record(imbalance=context.pos_spread)
+    #record(leverage=context.account.leverage)
+    if context.open_orders:
+        for orders in context.open_orders.iteritemts():
+            cancel_order(orders)
     for equity in context.portfolio.positions:  
         order_percent(equity, 0)
     context.x=True
+    price_history = data.history(context.underlying,"price",30,"1d")
+    compute_volatility(context,price_history)
+    r_value=np.corrcoef(context.volatility,context.performance)
+    corr=r_value[0][1]
+    record(corr=corr)
+    #print(corr)
+
+def compute_volatility(context,price_history):  
+    # Compute daily returns  
+    daily_returns = price_history.pct_change().dropna().values  
+    # Compute daily volatility  
+    historical_vol_daily = np.std(daily_returns,axis=0)
+    returns = context.portfolio.returns
+    context.performance.append(returns)
+    context.volatility.append(historical_vol_daily)
+    record(volatility=historical_vol_daily*10)    
     
 def allocate(context,data):
     if context.open_orders:
-        for orders in context.open_orders.iteritems():
+        for orders in context.open_orders.iteritemts():
             cancel_order(orders)
     bet_size = context.portfolio.portfolio_value * (context.truleverage-0.2)
     context.bull_trade_amt=-((0.5*bet_size)/(data.current(context.bull,'price')))-context.portfolio.positions[context.bull].amount
@@ -31,7 +56,7 @@ def allocate(context,data):
     order(context.bull,context.bull_trade_amt)
     order(context.bear,context.bear_trade_amt)
     context.x=False
-    
+        
 def handle_data(context,data):
     if len(context.portfolio.positions) > 0:
         bull_perc=abs((context.portfolio.positions[context.bull].amount*(data.current(context.bull,'price')))/(context.portfolio.portfolio_value))*100
@@ -40,7 +65,7 @@ def handle_data(context,data):
     if context.lever>context.truleverage:
             log.warn('Leverage Exceeded: '+str(context.lever))
             for equity in context.portfolio.positions:  
-                order_percent(equity, 0)
+                 order_percent(equity, 0)
             context.x=True
     if context.x==True:
         allocate(context,data)
